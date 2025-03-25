11cde1538f5a1feafd315fa9916047ce56c06da4
vsc47426 vscuser <vsc47426@gligar10.gastly.os>
2025-03-25 10:23:47 +0100

    failed endeavours

---
+++ split_tissue_function.py
@@ -0,0 +1,157 @@
+import pandas as pd
+import numpy as np
+import scanpy as sc 
+import matplotlib.pyplot as plt
+import os
+import argparse
+
+def split_tissue(input_pipeline, spatial_coords, n, figure=True, output_dir='output_files'):
+    ''' 
+        Splits tissue samples into a grid of n*n parts based on the spatial coordinates of the spots. 
+        This function generates n*n AnnData objects, an updated AnnData object with assigned parts, a CSV file containing the spots in each part, and optionally a figure showing the distribution of spots across the parts.
+
+        Args:
+            input_pipeline (str): File path to an AnnData object stored in .h5ad format, containing data for the spots.
+            spatial_coords (str): File path to a CSV file with 6 columns: 'spot_id', 'tissue', 'x0', 'y0', 'x', 'y', which define the spatial coordinates of the spots.
+            n (int): The number of divisions per dimension, with the tissue being split into an n*n grid of parts.
+            figure (bool, optional): If `True`, a figure will be displayed showing the distribution of spots across the parts (default is `True`).
+            output_dir (str, optional): Directory where the output files will be saved (default is 'output_files').
+
+        Returns:
+            None: The function saves the updated AnnData objects as .h5ad files, generates a CSV file with the spots' part assignments, 
+            and shows a figure if `figure=True`.
+
+        Notes:
+            - The parts are numbered from 0 to n*n-1. Part 0 contains the spots with the smallest x and y coordinates, part 1 contains the spots with the smallest x and the second smallest y coordinates, and so on.
+            - The function divides the spots evenly across the n*n grid based on their spatial coordinates.
+            - It checks whether all spots from the `input_pipeline` file are present in the `spatial_coords` dataset.
+            
+        Requirements:
+            - The function requires the following libraries to be installed: pandas, numpy, scanpy, matplotlib.
+    '''
+
+    # Create the output directory if it doesn't exist, and overwrite if it exists
+    if os.path.exists(output_dir):
+        # Remove existing files in the output directory before saving new ones
+        for filename in os.listdir(output_dir):
+            file_path = os.path.join(output_dir, filename)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+    else:
+        os.makedirs(output_dir)
+
+    # Load data
+    spatial_coords = pd.read_csv(spatial_coords, delimiter=',', names=['spot_id', 'tissue', 'x0', 'y0', 'x', 'y'])
+    input_pipeline = sc.read_h5ad(input_pipeline)
+    input_pipeline.obs['part'] = np.nan
+
+    # Check whether all spots in the input_pipeline file are present in the spatial_coords file
+    if not input_pipeline.obs.index.str[:-2].isin(spatial_coords['spot_id']).all():
+        print('Not all spots are present in the spatial_coords file')
+        return None
+    
+    # Filter the spatial_coords file to only include the spots present in the input_pipeline file
+    common_spots = input_pipeline.obs.index.str[:-2].intersection(spatial_coords.iloc[:,0])
+    spatial_coords_filtered = spatial_coords[spatial_coords['spot_id'].isin(common_spots)]
+
+    # Initialize part column with -1
+    count = 0
+
+    # Sort the spots by x and y coordinates
+    sorted_spots_x = spatial_coords_filtered.sort_values(['x0', 'y0'])
+
+    # Split the x and y coordinates into n equal parts
+    x_bins = np.array_split(sorted_spots_x, n, axis=0)
+
+    # Iterate through the bins to assign parts
+    for i in range(n):
+        x_subset = x_bins[i]
+
+        # Sort the subset by y and x for the next step
+        sorted_subset_y = x_subset.sort_values(['y', 'x'])
+        y_bins = np.array_split(sorted_subset_y, n, axis=0)
+
+        for j in range(n):            
+            # Get the spot ids that belong to this part
+            q = y_bins[j]['spot_id']
+
+            # Update the 'part' values in both input_pipeline and spatial_coords_filtered
+            input_pipeline.obs.loc[input_pipeline.obs.index.str[:-2].isin(q), 'part'] = count
+            spatial_coords_filtered.loc[spatial_coords_filtered['spot_id'].isin(q), 'part'] = count
+            count += 1
+
+    # Control whether all spots are assigned to a part
+    if sum(pd.isna(input_pipeline.obs['part'])) > 0:
+        print('Not all spots are assigned to a part')
+    else:
+        print('All spots are assigned to a part')
+
+    # Ensure the output directory exists
+    os.makedirs(output_dir, exist_ok=True)
+
+    # Write the updated AnnData object (AnnData object, containing the parts) to a .h5ad file
+    input_pipeline.__dict__['_raw'].__dict__['_var'] = input_pipeline.__dict__['_raw'].__dict__['_var'].rename(columns={'_index': 'features'})  # Needed otherwise there is a conflict
+    file_name2 = os.path.join(output_dir, "updated_annData_obj.h5ad")
+    input_pipeline.write(filename=file_name2)
+    print(f"Saved updated AnnData object to {file_name2}")
+
+    # Create a CSV file with the part each spot belongs to
+    df = pd.DataFrame(input_pipeline.obs['part'])
+    df.index = input_pipeline.obs['part'].index
+    csv_file = os.path.join(output_dir, "spots_and_parts.csv")
+    df.to_csv(csv_file)
+    print(f"Saved the index of the spots in each part to {csv_file}")
+
+    # Create a new AnnData object for each part
+    for part_value in sorted(input_pipeline.obs['part'].unique()):
+        part_data = input_pipeline[input_pipeline.obs['part'] == part_value].copy()
+        part_data.__dict__['_raw'].__dict__['_var'] = part_data.__dict__['_raw'].__dict__['_var'].rename(columns={'_index': 'features'})  # Needed otherwise there is a conflict
+        
+        # Save each part as a separate .h5ad file
+        part_file_name = os.path.join(output_dir, f"annData_part_{part_value}.h5ad")
+        part_data.write(filename=part_file_name)
+        print(f"Saved AnnData object for part {part_value} to {part_file_name}")
+
+
+    if figure == True:
+        # Calculate the count for each part
+        part_counts = input_pipeline.obs['part'].value_counts()
+
+        # Create a figure and axis
+        fig, ax = plt.subplots(figsize=(10, 5))
+
+        # Create the bar plot
+        bars = ax.bar(sorted(part_counts.index.astype(str)), part_counts.values, color='salmon')
+
+        # Set labels and title
+        ax.set_xlabel('Part')
+        ax.set_ylabel('Number of spots')
+        ax.set_title('Distribution of spots across parts')
+        ax.set_xticks(sorted(part_counts.index.astype(int)))
+        ax.set_xticklabels(sorted(part_counts.index.astype(int)))
+
+        # Display the height of each bar
+        for bar in bars:
+            yval = bar.get_height()
+            ax.text(bar.get_x() + bar.get_width() / 2, yval + 0.05, str(int(yval)), ha='center', va='bottom')
+
+        # Show the plot
+        plt.show()
+
+    print(input_pipeline.obs['part'])
+
+    return None
+
+def main():
+    parser = argparse.ArgumentParser(description="Split tissue into parts.")
+    parser.add_argument("input_pipeline", help="Path to AnnData .h5ad file")
+    parser.add_argument("spatial_coords", help="Path to spatial coordinates CSV file")
+    parser.add_argument("n", type=int, help="Number of divisions per dimension")
+    parser.add_argument("--figure", type=bool, default=True, help="Whether to show the figure")
+    parser.add_argument("--output_dir", default="output_files", help="Output directory")
+    args = parser.parse_args()
+
+    split_tissue(args.input_pipeline, args.spatial_coords, args.n, args.figure, args.output_dir)
+
+if __name__ == "__main__":
+    main()
--- subworkflows/deconvolution/cell2location/fit_model.py
+++ subworkflows/deconvolution/cell2location/fit_model.py
@@ -2,35 +2,38 @@
 import argparse as arp
 import os
 
+
 def main():
     ##### PARSING COMMAND LINE ARGUMENTS #####
     prs = arp.ArgumentParser()
-    
-    prs.add_argument('sp_data_path', type = str, help = 'path of spatial data')
 
-    prs.add_argument('model_path', type = str, help = 'path to regression model')
-    
-    prs.add_argument('cuda_device', type = str, help = "index of cuda device ID or cpu")
+    prs.add_argument('sp_data_path', type=str, help='path of spatial data')
 
-    prs.add_argument('-o','--out_dir', default = os.getcwd() ,
-                     type = str, help = 'model and proportions output directory')
+    prs.add_argument('model_path', type=str, help='path to regression model')
 
-    prs.add_argument('-e', '--epochs', default=30000, type = int, help = "number of epochs to fit the model")
+    prs.add_argument('cuda_device', type=str, help="index of cuda device ID or cpu")
 
-    prs.add_argument('-p', '--posterior_sampling', default=1000, type = int, help = "number of samples to take from the posterior distribution")
+    prs.add_argument('-o', '--out_dir', default=os.getcwd(),
+                     type=str, help='model and proportions output directory')
 
-    prs.add_argument('-n', '--n_cells_per_location', default=8, type = int, help = "estimated number of cells per spot")
+    prs.add_argument('-e', '--epochs', default=30000, type=int, help="number of epochs to fit the model")
 
-    prs.add_argument('-d', '--detection_alpha', default=200, type = int, help = "within-experiment variation in RNA detection sensitivity")
-    
+    prs.add_argument('-p', '--posterior_sampling', default=1000, type=int,
+                     help="number of samples to take from the posterior distribution")
+
+    prs.add_argument('-n', '--n_cells_per_location', default=8, type=int, help="estimated number of cells per spot")
+
+    prs.add_argument('-d', '--detection_alpha', default=200, type=int,
+                     help="within-experiment variation in RNA detection sensitivity")
+
     args = prs.parse_args()
-    
+
     cuda_device = args.cuda_device
     sp_data_path = args.sp_data_path
     output_folder = args.out_dir
-        
+
     assert (cuda_device.isdigit() or cuda_device == "cpu"), "invalid device input"
-    
+
     print("Parameters\n==========")
     print("Detection alpha: {}\nCells per location: {}".format(args.detection_alpha, args.n_cells_per_location))
     print("Training epochs: {}\nPosterior sampling: {}".format(args.epochs, args.posterior_sampling))
@@ -38,22 +41,17 @@ def main():
 
     ##### MAIN PART #####
     if cuda_device.isdigit():
-        os.environ["CUDA_VISIBLE_DEVICES"]=cuda_device
+        os.environ["CUDA_VISIBLE_DEVICES"] = cuda_device
 
-    import sys
+
     import scanpy as sc
-    import anndata
-    import pandas as pd
     import numpy as np
 
     import cell2location
     import scvi
 
-    import matplotlib as mpl
     from matplotlib import rcParams
     rcParams['pdf.fonttype'] = 42
-    import matplotlib.pyplot as plt
-    import seaborn as sns
 
     # silence scanpy that prints a lot of warnings
     import warnings
@@ -72,13 +70,14 @@ def main():
 
     print("Reading in the model...")
     adata_scrna_raw = sc.read(args.model_path)
-    
+
     # Export estimated expression in each cluster
     if 'means_per_cluster_mu_fg' in adata_scrna_raw.varm.keys():
-        inf_aver = adata_scrna_raw.varm['means_per_cluster_mu_fg'][[f'means_per_cluster_mu_fg_{i}' 
-                                        for i in adata_scrna_raw.uns['mod']['factor_names']]].copy()
+        inf_aver = adata_scrna_raw.varm['means_per_cluster_mu_fg'][[f'means_per_cluster_mu_fg_{i}'
+                                                                    for i in
+                                                                    adata_scrna_raw.uns['mod']['factor_names']]].copy()
     else:
-        inf_aver = adata_scrna_raw.var[[f'means_per_cluster_mu_fg_{i}' 
+        inf_aver = adata_scrna_raw.var[[f'means_per_cluster_mu_fg_{i}'
                                         for i in adata_scrna_raw.uns['mod']['factor_names']]].copy()
     inf_aver.columns = adata_scrna_raw.uns['mod']['factor_names']
 
@@ -87,49 +86,115 @@ def main():
     adata_vis = adata_vis[:, intersect].copy()
     inf_aver = inf_aver.loc[intersect, :].copy()
 
-    # prepare anndata for cell2location model
-    scvi.data.setup_anndata(adata=adata_vis)
+    #new
+    chunk_size = 72_000
+    chunks = [i for i in range(int(np.ceil(adata_vis.n_obs / chunk_size)))]
+    #prints chuncks
+    print("splitting into: "+str(chunks))
+    print("Columns in adata_vis.obs:", adata_vis.obs.columns.tolist())
+    adata_vis.obs['training_batch'] = 0
+    for sample in adata_vis.obs['orig.ident'].unique():
+        ind = adata_vis.obs['orig.ident'].isin([sample])
+        adata_vis.obs.loc[ind, 'training_batch'] = np.random.choice(
+            chunks, size=ind.sum(), replace=True, p=None
+        )
 
-    # Create and train the model
-    mod = cell2location.models.Cell2location(
-        adata_vis, cell_state_df=inf_aver, 
-        # the expected average cell abundance: tissue-dependent 
-        # hyper-prior which can be estimated from paired histology:
-        N_cells_per_location=args.n_cells_per_location,
-        # hyperparameter controlling normalisation of
-        # within-experiment variation in RNA detection (using default here):
-        detection_alpha=args.detection_alpha
-    ) 
+    adata_vis_full = adata_vis.copy()
+    for k in ['means', 'stds', 'q05', 'q95']:
+        adata_vis_full.obsm[f"{k}_cell_abundance_w_sf"] = np.zeros((adata_vis_full.n_obs, inf_aver.shape[1]))
 
-    mod.train(max_epochs=args.epochs, 
-            # train using full data (batch_size=None)
-            batch_size=None, 
-            # use all data points in training because 
-            # we need to estimate cell abundance at all locations
-            train_size=1,
-            use_gpu=cuda_device.isdigit())
+    adata_vis.obs['training_batch'].value_counts()
+    print("Training batches NEW:")
+    print(adata_vis.obs['training_batch'].value_counts())
 
-    # Export the estimated cell abundance (summary of the posterior distribution).
-    adata_vis = mod.export_posterior(
-        adata_vis, sample_kwargs={'num_samples': args.posterior_sampling,
-        'batch_size': mod.adata.n_obs, 'use_gpu': cuda_device.isdigit()}
-    )
+    seed = 0
+    scvi.settings.seed = seed
+    np.random.seed(seed)
 
-    # Save model and anndata object with results
-    mod.save(output_folder , overwrite=True)
-    adata_vis.write(os.path.join(output_folder, 'sp.h5ad'))
+    # submit this chunk as separate jobs
+    for batch in adata_vis.obs['training_batch'].unique():
+        # create and train the model
+        scvi_run_name = f'fit_model_batch{batch}_seed{seed}'
+        print(scvi_run_name)
 
-    # Export proportion file, but first rename columns and divide by rowSums
-    props = adata_vis.obsm['q05_cell_abundance_w_sf']
-    props = props.rename(columns={x:x.replace("q05cell_abundance_w_sf_", "") for x in props.columns})
-    props = props.div(props.sum(axis=1), axis='index')
-    props.to_csv(os.path.join(output_folder, 'proportions.tsv'), sep="\t")
+        training_batch_index = adata_vis_full.obs['training_batch'].isin([batch])
+        adata_vis = adata_vis_full[training_batch_index, :].copy()
+
+        # prepare anndata for scVI model
+        cell2location.models.Cell2location.setup_anndata(adata_vis, batch_key="orig.ident")
+
+        # Step 2: Instantiate the Cell2location model.
+        mod = cell2location.models.Cell2location(
+            adata_vis,
+            cell_state_df=inf_aver,  # reference signatures from your scRNA-seq data
+            N_cells_per_location=args.n_cells_per_location,
+            detection_alpha=args.detection_alpha
+        )
+
+        # Now you can train the model.
+        mod.train(max_epochs=args.epochs, batch_size=None, train_size=1, use_gpu=cuda_device.isdigit())
+
+        # export posterior
+        # In this section, we export the estimated cell abundance (summary of the posterior distribution).
+
+        adata_vis = mod.export_posterior(
+    		adata_vis,
+    		sample_kwargs={
+  	     	'num_samples': args.posterior_sampling,
+        	'batch_size': mod.adata.n_obs,
+        	'use_gpu': cuda_device.isdigit()
+    	},
+    	add_to_obsm=['q05']
+	)
+
+    for k in adata_vis_full.obsm.keys():
+        adata_vis_full.obsm[k][training_batch_index, :] = adata_vis.obsm[k].copy()
+    adata_vis_full.uns[f'mod_{batch}'] = adata_vis.uns['mod'].copy()
+
+
+    # # prepare anndata for cell2location model
+    # scvi.data.setup_anndata(adata=adata_vis)
+    #
+    # # Create and train the model
+    # mod = cell2location.models.Cell2location(
+    #     adata_vis, cell_state_df=inf_aver,
+    #     # the expected average cell abundance: tissue-dependent
+    #     # hyper-prior which can be estimated from paired histology:
+    #     N_cells_per_location=args.n_cells_per_location,
+    #     # hyperparameter controlling normalisation of
+    #     # within-experiment variation in RNA detection (using default here):
+    #     detection_alpha=args.detection_alpha
+    # )
+    #
+    # mod.train(max_epochs=args.epochs,
+    #           # train using full data (batch_size=None)
+    #           batch_size=None,
+    #           # use all data points in training because
+    #           # we need to estimate cell abundance at all locations
+    #           train_size=1,
+    #           use_gpu=cuda_device.isdigit())
+    #
+    # # Export the estimated cell abundance (summary of the posterior distribution).
+    # adata_vis = mod.export_posterior(
+    #     adata_vis, sample_kwargs={'num_samples': args.posterior_sampling,
+    #                               'batch_size': mod.adata.n_obs, 'use_gpu': cuda_device.isdigit()}
+    # )
+    #
+    # # Save model and anndata object with results
+    # mod.save(output_folder, overwrite=True)
+    # adata_vis.write(os.path.join(output_folder, 'sp.h5ad'))
+    #
+    # # Export proportion file, but first rename columns and divide by rowSums
+    # props = adata_vis.obsm['q05_cell_abundance_w_sf']
+    # props = props.rename(columns={x: x.replace("q05cell_abundance_w_sf_", "") for x in props.columns})
+    # props = props.div(props.sum(axis=1), axis='index')
+    # props.to_csv(os.path.join(output_folder, 'proportions.tsv'), sep="\t")
 
     # df = pd.DataFrame(data=np.random.normal(size=(10,10)),
     #                     index=["row"+str(i) for i in range(10)],
     #                     columns=["col"+str(i) for i in range(10)])
     # df.to_csv(os.path.join(output_folder, 'proportions.tsv'), sep="\t")
 
-        
+
 if __name__ == '__main__':
     main()
